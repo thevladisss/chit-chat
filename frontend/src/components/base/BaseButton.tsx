9b529dd85dbec3647d28695470590eec674d678f
Vladyslav Nosal <vladyslavnosal@gmail.com>
2025-07-05 11:30:29 -0700

    Add icon mode to BaseButton

--- frontend/src/components/base/BaseButton.tsx
+++ frontend/src/components/base/BaseButton.tsx
@@ -5,21 +5,24 @@ import classNames from "classnames";
 type Props = HTMLProps<HTMLButtonElement> &
   PropsWithChildren<{
     loading?: boolean;
+    icon?: JSX.Element;
   }>;
 function BaseButton({
   style,
   className,
   type,
   children,
+  icon,
   loading,
 }: Props): JSX.Element {
   const classes = classNames("base-button", className, {
     loading: loading,
+    icon: Boolean(icon),
   });
 
   return (
     <button type={type} style={style} className={classes} disabled={loading}>
-      {children}
+      {icon ? icon : children}
     </button>
   );
 }
