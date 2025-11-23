4eda877403b70ca3334a0fa939a436e44498e4e2
Vladyslav Nosal <vladyslavnosal@gmail.com>
2025-11-23 12:35:52 -0800

    refactor: move WS logic to layout level

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -4,15 +4,104 @@ import { useUser } from "../hooks/useUser.tsx";
 import { AUTH_PATH } from "../constants/route-paths.ts";
 import UserSidebar from "../components/UserSidebar.tsx";
 import "./AppLayout.css";
+import { useEffect, useRef } from "react";
+import { ServerSideEventsEnum } from "../enums/ServerSideEventsEnum.ts";
+import { useChatStore } from "../hooks/useChatStore.ts";
+import { IUser } from "../types/IUser.ts";
+import { IWSMessageEventData } from "../types/ws/IWSMessageEventData.ts";
+
+const messageSound = new Audio("/sounds/message.mp3");
 
 function AppLayout() {
-  const navigate = useNavigate();
-  const { signOutUser } = useUser();
-
-  const handleSignOut = () => {
-    signOutUser();
-    navigate(AUTH_PATH);
+  const playMessageSound = async () => {
+    await messageSound.play();
   };
+
+  const { getChats, setChats, setTypingChat, deleteTypingChat } =
+    useChatStore();
+
+  const typingTimeout = useRef<Record<string, NodeJS.Timeout>>({});
+
+  let ws: WebSocket | null = null;
+
+  useEffect(() => {
+    if (!ws) {
+      ws = new WebSocket(import.meta.env.VITE_WS_URL || "ws://localhost:3000");
+      window.ws = ws;
+    }
+
+    ws.onopen = () => {
+      getChats();
+    };
+
+    type WsCustomEvent<R = any> = { event: string; data: R };
+
+    const handleConnectionEvent = (e: WsCustomEvent) => {
+      setChats(e.data.chats);
+    };
+
+    const handleLeaveConnectionEvent = (e: WsCustomEvent) => {
+      setChats(e.data.chats);
+    };
+
+    const handleNewUserEvent = (e: WsCustomEvent) => {
+      getChats();
+    };
+
+    const TYPING_CLEAR_TIMEOUT = 2000;
+
+    const handleTypingInChatEvent = (e: WsCustomEvent) => {
+      const chatId = e.data.chatId as string,
+        user = e.data.user as IUser;
+
+      setTypingChat(chatId, [user]);
+
+      if (typingTimeout.current[chatId]) {
+        clearTimeout(typingTimeout.current[chatId]);
+      }
+
+      typingTimeout.current[chatId] = setTimeout(() => {
+        deleteTypingChat(chatId);
+      }, TYPING_CLEAR_TIMEOUT);
+    };
+
+    const handleChatCreatedEvent = (e: WsCustomEvent) => {
+      setChats(e.data);
+    };
+
+    const handleMessageEvent = (e: WsCustomEvent<IWSMessageEventData>) => {
+      if (!e.data.isSenderSelf) {
+        playMessageSound();
+      }
+
+      setChats(e.data.chats);
+    };
+
+    ws.onmessage = (e: MessageEvent) => {
+      const payload = JSON.parse(e.data) as WsCustomEvent;
+
+      switch (payload.event) {
+        case ServerSideEventsEnum.Connection:
+          handleConnectionEvent(payload);
+          break;
+        case ServerSideEventsEnum.LeaveConnection:
+          handleLeaveConnectionEvent(payload);
+          break;
+        case ServerSideEventsEnum.Message:
+          handleMessageEvent(payload);
+          break;
+        case ServerSideEventsEnum.ChatCreated:
+          handleChatCreatedEvent(payload);
+          break;
+        case ServerSideEventsEnum.NewUser:
+          handleNewUserEvent(payload);
+          break;
+        case ServerSideEventsEnum.TypingInChat:
+          handleTypingInChatEvent(payload);
+          break;
+      }
+    };
+  }, []);
 
   return (
     <div className="app-layout">
--- frontend/src/views/ChatView.tsx
+++ frontend/src/views/ChatView.tsx
@@ -6,27 +6,11 @@ import ChatComposer from "../components/ChatComposer.tsx";
 import { useChatStore } from "../hooks/useChatStore.ts";
 import { useTimer } from "../hooks/useTimer.ts";
 import { useDateTime } from "../hooks/useDateTime.ts";
-import { ServerSideEventsEnum } from "../enums/ServerSideEventsEnum.ts";
-import { IWSMessageEventData } from "../types/ws/IWSMessageEventData.ts";
 import { useAudioRecording } from "../hooks/useAudioRecording.ts";
 import ChatStatusBar from "../components/ChatStatusBar.tsx";
-import { IUser } from "../types/IUser.ts";
-
-declare global {
-  interface Window {
-    ws?: WebSocket;
-  }
-}
-
-const messageSound = new Audio("/sounds/message.mp3");
 
 function ChatView() {
-  const playMessageSound = async () => {
-    await messageSound.play();
-  };
-
-  const { getChats, selectedChat, setChats, setTypingChat, deleteTypingChat } =
-    useChatStore();
+  const { selectedChat, setChats } = useChatStore();
 
   const selectedChatName = useMemo(
     () => (selectedChat ? selectedChat.name : ""),
@@ -39,94 +23,6 @@ function ChatView() {
     () => (selectedChat ? selectedChat.users.length : null),
     [selectedChat]
   );
-
-  const typingTimeout = useRef<Record<string, NodeJS.Timeout>>({});
-
-  let ws: WebSocket | null = null;
-
-  useEffect(() => {
-    if (!ws) {
-      ws = new WebSocket(import.meta.env.VITE_WS_URL || "ws://localhost:3000");
-      (window as any).ws = ws;
-    }
-
-    ws.onopen = () => {
-      requestChats();
-    };
-
-    type WsCustomEvent<R = any> = { event: string; data: R };
-
-    const handleConnectionEvent = (e: WsCustomEvent) => {
-      setChats(e.data.chats);
-    };
-
-    const handleLeaveConnectionEvent = (e: WsCustomEvent) => {
-      setChats(e.data.chats);
-    };
-
-    const handleNewUserEvent = (e: WsCustomEvent) => {
-      requestChats();
-    };
-
-    const TYPING_CLEAR_TIMEOUT = 2000;
-
-    const handleTypingInChatEvent = (e: WsCustomEvent) => {
-      const chatId = e.data.chatId as string,
-        userId = e.data.userId as string,
-        user = e.data.user as IUser;
-
-      setTypingChat(chatId, [user]);
-
-      if (typingTimeout.current[chatId]) {
-        clearTimeout(typingTimeout.current[chatId]);
-      }
-
-      typingTimeout.current[chatId] = setTimeout(() => {
-        deleteTypingChat(chatId);
-      }, TYPING_CLEAR_TIMEOUT);
-    };
-
-    const handleChatCreatedEvent = (e: WsCustomEvent) => {
-      setChats(e.data);
-    };
-
-    const handleMessageEvent = (e: WsCustomEvent<IWSMessageEventData>) => {
-      if (!e.data.isSenderSelf) {
-        playMessageSound();
-      }
-
-      setChats(e.data.chats);
-    };
-
-    ws.onmessage = (e: MessageEvent) => {
-      const payload = JSON.parse(e.data) as WsCustomEvent;
-
-      switch (payload.event) {
-        case ServerSideEventsEnum.Connection:
-          handleConnectionEvent(payload);
-          break;
-        case ServerSideEventsEnum.LeaveConnection:
-          handleLeaveConnectionEvent(payload);
-          break;
-        case ServerSideEventsEnum.Message:
-          handleMessageEvent(payload);
-          break;
-        case ServerSideEventsEnum.ChatCreated:
-          handleChatCreatedEvent(payload);
-          break;
-        case ServerSideEventsEnum.NewUser:
-          handleNewUserEvent(payload);
-          break;
-        case ServerSideEventsEnum.TypingInChat:
-          handleTypingInChatEvent(payload);
-          break;
-      }
-    };
-  }, []);
-
-  const requestChats = async () => {
-    getChats();
-  };
 
   const [message, setMessageInput] = useState("");
 
