7660be46bb0569c1d02de94f9182f1b218384d85
Vladyslav Nosal <vladyslavnosal@gmail.com>
2025-11-30 21:18:42 -0800

    feat: set default format for formatUnixTimestamp

--- frontend/src/utils/dateTimeUtil.ts
+++ frontend/src/utils/dateTimeUtil.ts
@@ -1,5 +1,8 @@
 import dayjs from "dayjs";
 
-export const formatUnixTimestamp = (timestamp: number) => {
-  return dayjs(timestamp).toString();
+export const formatUnixTimestamp = (
+  timestamp: number,
+  format = "MM/DD/YYYY HH:mm"
+) => {
+  return dayjs(timestamp).format(format).toString();
 };
