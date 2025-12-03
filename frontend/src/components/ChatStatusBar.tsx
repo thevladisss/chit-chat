e7b27572b1bef24d5d25a3544b32e5e6fcc3bd60
Vladyslav Nosal <vladyslavnosal@gmail.com>
2025-12-02 21:06:08 -0800

    feat: remove display of back button in ChatStatusBar for large screens

--- frontend/src/components/ChatStatusBar.tsx
+++ frontend/src/components/ChatStatusBar.tsx
@@ -2,20 +2,24 @@ import Icon from "@mdi/react";
 import "./ChatStatusBar.css";
 import BaseButton from "./base/BaseButton.tsx";
 import { mdiArrowLeft } from "@mdi/js";
+import { useScreen } from "../hooks/useScreen.ts";
 
 export default function ChatStatusBar({
   chatName,
   participantsCount,
   onLeaveChat,
 }: any) {
+  const { smAndSmaller } = useScreen();
   return (
     <div className="chat-status-bar">
-      <div className="actions">
-        <BaseButton
-          onClick={() => onLeaveChat()}
-          icon={<Icon path={mdiArrowLeft} size={1} title="Leave" />}
-        />
-      </div>
+      {smAndSmaller && (
+        <div className="actions">
+          <BaseButton
+            onClick={() => onLeaveChat()}
+            icon={<Icon path={mdiArrowLeft} size={1} title="Leave" />}
+          />
+        </div>
+      )}
       <div className="chat-status-bar-info">
         <h2>{chatName}</h2>
         <p>{participantsCount} members</p>
