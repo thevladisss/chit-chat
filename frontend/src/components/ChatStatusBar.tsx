d6d1272959b1ab10115f6a309a8aabb52ddf4dfe
Vladyslav Nosal <vladyslavnosal@gmail.com>
2026-01-10 20:09:06 -0800

    feat: add video and audio call buttons in chat bar

--- frontend/src/components/ChatStatusBar.tsx
+++ frontend/src/components/ChatStatusBar.tsx
@@ -1,20 +1,30 @@
 import Icon from "@mdi/react";
 import "./ChatStatusBar.css";
 import BaseButton from "./base/BaseButton.tsx";
-import { mdiArrowLeft } from "@mdi/js";
+import { mdiArrowLeft, mdiPhone, mdiVideo } from "@mdi/js";
 import { useScreen } from "../hooks/useScreen.ts";
+
+type Props = {
+  chatName: string;
+  participantsCount: number;
+  onLeaveChat: () => void;
+  onVideoCall: () => void;
+  onAudioCall: () => void;
+};
 
 export default function ChatStatusBar({
   chatName,
   participantsCount,
   onLeaveChat,
-}: any) {
+  onVideoCall,
+  onAudioCall,
+}: Props) {
   const { smAndSmaller } = useScreen();
   return (
     <div className="chat-status-bar">
       {smAndSmaller && (
         <div className="actions">
-          <BaseButton
+        <BaseButton
             onClick={() => onLeaveChat()}
             icon={<Icon path={mdiArrowLeft} size={1} title="Leave" />}
           />
@@ -23,6 +33,16 @@ export default function ChatStatusBar({
       <div className="chat-status-bar-info">
         <h2>{chatName}</h2>
         <p>{participantsCount} members</p>
+      </div>
+      <div className="actions">
+        <BaseButton
+          onClick={() => onAudioCall()}
+          icon={<Icon path={mdiPhone} size={1} title="Video Call" />}
+        />
+        <BaseButton
+          onClick={() => onVideoCall()}
+          icon={<Icon path={mdiVideo} size={1} title="Video Call" />}
+        />
       </div>
     </div>
   );
